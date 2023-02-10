7e2b70568038775aa13555d9d71a86bdbf01b929
BlueNexus6674 <lwalker6674@gmail.com>
2023-02-10 13:51:02 +0000

    Adding custom_isaac_repo

---
+++ custom_isaac_repo/setup.py
@@ -0,0 +1,28 @@
+"""Installation script for the 'isaacgymenvs' python package."""
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+from setuptools import setup, find_packages
+
+import os
+
+# Minimum dependencies required prior to installation
+INSTALL_REQUIRES = []
+
+# Installation operation
+setup(
+    name="custom_isaac_repo",
+    author="LW",
+    version="1.0.0",
+    description="Unsupport RPMFlow",
+    keywords=["RPM"],
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages("."),
+    classifiers=["Natural Language :: English", "Programming Language :: Python :: 3.7, 3.8"],
+    zip_safe=False,
+)
+
+# EOF
