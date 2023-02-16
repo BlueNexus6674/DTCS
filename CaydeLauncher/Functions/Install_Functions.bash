58d0d88dc2bb24812e18c290214eaaa191f5ccfe
Nexus <65248566+BlueNexus6674@users.noreply.github.com>
2023-02-16 15:32:01 +0000

    Update Install_Functions.bash
    Installs now assume yes

--- CaydeLauncher/Functions/Install_Functions.bash
+++ CaydeLauncher/Functions/Install_Functions.bash
@@ -7,7 +7,7 @@ Install_CaydeLauncher () {
 	echo "Installing CaydeLauncher"
 	echo ""
 	DTCSRepoLauncherPath=${DTCSRepoPath}/CaydeLauncher/Config/CaydeLauncher.desktop
-	DestinationLauncherPath=~/.local/share/applications/CaydeLauncher.desktop
+	DestinationLauncherPath=/home/${UserName}/.local/share/applications/CaydeLauncher.desktop
 	cp $DTCSRepoLauncherPath $DestinationLauncherPath
 }
 
@@ -23,7 +23,8 @@ Install_Linux_Config () {
 	echo ""
 	echo "Config Linux (Updating)"
 	echo ""
-	sudo apt update && sudo apt upgrade
+	sudo apt update
+	sudo apt upgrade -y
 	
 	echo "${DTCSRepoPath}/ROS_Workspaces/ros_workspace/devel/setup.bash" >> ~/.bashrc
 }
@@ -39,24 +40,24 @@ Install_ROS_Noetic () {
 	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 	 
 	#Set up your keys
-	sudo apt install curl
+	sudo apt -y install curl
 	curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 	#Update Package Index
 	sudo apt update
 
 	#Installation
-	sudo apt install ros-noetic-desktop-full
+	sudo apt -y install ros-noetic-desktop-full
 	 
 	#Source ROS
 	source /opt/ros/noetic/setup.bash
 	echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 
 	#Dependencies for building packages
-	sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+	sudo apt -y install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 
 	#Initialize rosdep
-	sudo apt install python3-rosdep
+	sudo apt -y install python3-rosdep
 	sudo rosdep init
 	rosdep update
 }
@@ -67,14 +68,16 @@ Install_ROS2_Foxy () {
 	echo ""
 	#Instructions from https://docs.ros.org/en/foxy/Installation/Ubuntu-Install-Debians.html
 
-	sudo apt update && sudo apt upgrade
+	sudo apt update
+	sudo apt upgrade -y
 
 	#Setup sources
-	sudo apt install software-properties-common
+	sudo apt -y install software-properties-common
 	sudo add-apt-repository universe
 	 
 	#Add ROS2 GPG Key
-	sudo apt update && sudo apt install curl
+	sudo apt update
+	sudo apt -y install curl
 	sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 	 
 	#Add repository to sources list
@@ -82,18 +85,18 @@ Install_ROS2_Foxy () {
 	 
 	#Install ROS2 packages
 	sudo apt update
-	sudo apt upgrade
-	sudo apt install ros-foxy-desktop python3-argcomplete
+	sudo apt upgrade -y
+	sudo apt -y install ros-foxy-desktop python3-argcomplete
 
 	#Source setup script
 	#source /opt/ros/foxy/setup.bash
 	#echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
 	
 	#For rosdep install command
-	sudo apt install ros-dev-tools
+	sudo apt -y install ros-dev-tools
 	 
 	#For colcon build command
-	sudo apt install python3-colcon-common-extensions
+	sudo apt -y install python3-colcon-common-extensions
 }
 
 Install_UR_ROS_Noetic_Driver () {
@@ -184,9 +187,9 @@ Install_IsaacSIM_Gym_Env () {
 	echo ""
 	echo "IsaacSIM_Gym_Env Install"
 	echo ""
-	cd ~
+	cd /home/${UserName}/
 	git clone https://github.com/NVIDIA-Omniverse/OmniIsaacGymEnvs.git
-	cd ~/OmniIsaacGymEnvs/
+	cd /home/${UserName}/OmniIsaacGymEnvs/
 	$IsaacPythonPath -m pip install -e .
 }
 
@@ -247,13 +250,13 @@ Install_ROS_Control () {
 	echo ""
 	echo "ROS Control Install"
 	echo ""
-	sudo apt-get install ros-noetic-ros-control ros-noetic-ros-controllers
+	sudo apt-get -y install ros-noetic-ros-control ros-noetic-ros-controllers
 }
 
 Install_ROS_Moveit () {
 	echo ""
 	echo "ROS Moveit Install"
 	echo ""
-	sudo apt install ros-noetic-moveit
+	sudo apt -y install ros-noetic-moveit
 }
 
