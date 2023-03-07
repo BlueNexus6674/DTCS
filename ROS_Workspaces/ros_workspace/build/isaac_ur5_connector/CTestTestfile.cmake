dd44df00d2198abd7a86495563c6e391909f8343
BlueNexus6674 <lwalker6674@gmail.com>
2023-03-07 04:54:03 +0000

    a

--- ROS_Workspaces/ros_workspace/build/isaac_ur5_connector/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cayde/Documents/DTCS/ROS_Workspaces/ros_workspace/src/isaac_ur5_connector
-# Build directory: /home/cayde/Documents/DTCS/ROS_Workspaces/ros_workspace/build/isaac_ur5_connector
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ROS_Workspaces/ros_workspace/build/ur5withrg2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cayde/Documents/DTCS/ROS_Workspaces/ros_workspace/src/ur5withrg2
-# Build directory: /home/cayde/Documents/DTCS/ROS_Workspaces/ros_workspace/build/ur5withrg2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
